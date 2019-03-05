cf0c4a6d5bc4b779af0836daa776b3b7ac2ae493
Nhi <nhinhtse61797@fpt.edu.vn>
2019-03-05 13:27:24 +0700

    create/update item - updated

---
+++ app/src/main/java/com/project/capstone/exchangesystem/Activity/UpdateItemActivity.java
@@ -0,0 +1,420 @@
+package com.project.capstone.exchangesystem.Activity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.project.capstone.exchangesystem.R;
+import com.project.capstone.exchangesystem.Utils.RmaAPIUtils;
+import com.project.capstone.exchangesystem.model.Category;
+import com.project.capstone.exchangesystem.model.Image;
+import com.project.capstone.exchangesystem.model.Item;
+import com.project.capstone.exchangesystem.remote.RmaAPIService;
+import com.squareup.picasso.Picasso;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.util.*;
+
+public class UpdateItemActivity extends AppCompatActivity {
+
+    private static final int GALLERY_REQUEST = 2;
+    private final String PRIVACY_PUBLIC = "Công khai";
+    private final String PRIVACY_FRIENDS = "Bạn bè";
+    private final int IMAGE_SIZE = 160;
+    private final int IMAGE_MARGIN_TOP_RIGHT = 10;
+    private final int ADD_IMAGE_FLAG = 1;
+    private final int CHANGE_IMAGE_FLAG = 0;
+    TextView lblToolbar;
+    Spinner spCategory;
+    RmaAPIService rmaAPIService;
+    List<String> categoryList, privacyList, urlList;
+    List<ImageView> imageList;
+    Button btnUpdate, btnAddImage;
+    ImageView tmpImage;
+    EditText edtItemName, edtItemDes, edtItemAddress;
+    Spinner spPrivacy;
+    Context context;
+    ArrayAdapter<String> dataAdapter;
+    String imagePath, authorization;
+    int itemId, onClickFlag = -1, selectedPosition;
+    SharedPreferences sharedPreferences;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+    FirebaseAuth fbAuth;
+    FirebaseUser fbUser;
+    List<Uri> selectedImages;
+    Uri selectedImage;
+    GridLayout gridLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_item);
+        context = this;
+        getComponents();
+        getAllCategory();
+        getAllPrivacy();
+
+        sharedPreferences = getSharedPreferences("localData", MODE_PRIVATE);
+        authorization = sharedPreferences.getString("authorization", null);
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+        fbAuth = FirebaseAuth.getInstance();
+        fbUser = fbAuth.getCurrentUser();
+
+        imageList = new ArrayList<>();
+        urlList = new ArrayList<>();
+        //list uri
+        selectedImages = new ArrayList<>();
+
+        Intent intent = getIntent();
+        itemId = intent.getIntExtra("itemId", 0);
+        loadItem();
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                checkLoginFirebase();
+                updateItem();
+            }
+        });
+
+        btnAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickFlag = ADD_IMAGE_FLAG;
+                getImageFromGallery();
+            }
+        });
+    }
+
+    private void checkLoginFirebase() {
+        if (fbUser != null) {
+            uploadImagesToFireBase();
+        } else {
+            fbAuth.signInAnonymously().addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText(UpdateItemActivity.this, "Login successfully", Toast.LENGTH_SHORT).show();
+                    uploadImagesToFireBase();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(UpdateItemActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void getAllPrivacy() {
+        privacyList = new ArrayList<>();
+        privacyList.add(PRIVACY_PUBLIC);
+        privacyList.add(PRIVACY_FRIENDS);
+        setDataForSpinner(spPrivacy, privacyList);
+    }
+
+    private void setDataForSpinner(Spinner spinner, List<String> dataArray) {
+        dataAdapter = new ArrayAdapter<>(context, R.layout.spinner_category_item, dataArray);
+        dataAdapter.setDropDownViewResource(R.layout.spinner_category_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
+    private void getAllCategory() {
+        Toast.makeText(getApplicationContext(), "getAllCategory", Toast.LENGTH_LONG).show();
+        rmaAPIService.getAllCategory().enqueue(new Callback<List<Category>>() {
+            @Override
+            public void onResponse(Call<List<Category>> call, Response<List<Category>> response) {
+                Toast.makeText(getApplicationContext(), "Test category!!!!!", Toast.LENGTH_LONG).show();
+                if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        List<Category> result = response.body();
+                        categoryList = new ArrayList<>();
+                        for (int i = 0; i < result.size(); i++) {
+                            categoryList.add(result.get(i).getName());
+                            System.out.println("Cate 1: " + categoryList.get(i));
+                        }
+                        Toast.makeText(getApplicationContext(), "list size " + categoryList.size(), Toast.LENGTH_LONG).show();
+                        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(context, R.layout.spinner_category_item, categoryList);
+                        dataAdapter.setDropDownViewResource(R.layout.spinner_category_item);
+                        spCategory.setAdapter(dataAdapter);
+                    } else {
+                        System.out.println("httpstatus " + response.code());
+                        Toast.makeText(getApplicationContext(), "body null", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed category", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Category>> call, Throwable t) {
+                System.out.println("Fail");
+                Toast.makeText(getApplicationContext(), "AAAAAAAAAAA", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void getImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        String[] imageTypes = {"image/jpeg", "image/png", "image/jpg"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, imageTypes);
+
+        if (onClickFlag == ADD_IMAGE_FLAG) {
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        }
+
+        startActivityForResult(intent, GALLERY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK && data != null) {
+            if (onClickFlag == ADD_IMAGE_FLAG) {
+                if (data.getClipData() != null) {
+                    for (int i = 0; i < data.getClipData().getItemCount(); i++) {
+                        selectedImages.add(data.getClipData().getItemAt(i).getUri());
+                        createImageView();
+                    }
+                }
+            } else if (onClickFlag == CHANGE_IMAGE_FLAG) {
+                if (data.getData() != null) {
+                    selectedImages.set(selectedPosition, data.getData());
+                    try {
+                        Bitmap bmp = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImages.get(selectedPosition));
+                        tmpImage.setImageBitmap(bmp);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private void uploadImagesToFireBase() {
+        if (selectedImage != null) {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Đang tải...");
+            progressDialog.show();
+
+            StorageReference reference = storageReference.child("images/" + UUID.randomUUID().toString());
+
+            reference.putFile(selectedImage).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        progressDialog.dismiss();
+                        Toast.makeText(UpdateItemActivity.this, "Tải thành công", Toast.LENGTH_SHORT).show();
+                        task.getResult().getMetadata().getReference().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                imagePath = uri.toString(); //get url
+                                if (onClickFlag == 0) { //change url
+                                    urlList.set(imageList.indexOf(tmpImage), imagePath);
+                                } else { //add url
+                                    urlList.add(imagePath);
+                                }
+                            }
+                        });
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    Toast.makeText(UpdateItemActivity.this, "Tải thất bại" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                    progressDialog.setMessage("Uploaded " + (int) progress + "%");
+                }
+            });
+        }
+    }
+
+    private void loadItem() {
+        if (authorization != null) {
+            rmaAPIService.getImagesByItemId(authorization, itemId).enqueue(new Callback<List<Image>>() {
+                @Override
+                public void onResponse(Call<List<Image>> call, Response<List<Image>> response) {
+                    if (response.isSuccessful()) {
+                        if (response.body() != null) {
+                            for (int i = 0; i < response.body().size(); i++) {
+                                urlList.add(response.body().get(i).getUrl());
+                                createImageView();
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "image null", Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "load image failed", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<Image>> call, Throwable t) {
+                    Toast.makeText(getApplicationContext(), "load image failed!!!", Toast.LENGTH_LONG).show();
+                }
+            });
+            rmaAPIService.getItemById(authorization, itemId).enqueue(new Callback<Item>() {
+                @Override
+                public void onResponse(Call<Item> call, Response<Item> response) {
+                    if (response.isSuccessful()) {
+                        if (response.body() != null) {
+                            edtItemName.setText(response.body().getName());
+                            edtItemDes.setText(response.body().getDescription());
+                            edtItemAddress.setText(response.body().getAddress());
+                            spPrivacy.setSelection(Integer.parseInt(response.body().getPrivacy()));
+                            spCategory.setSelection((response.body().getCategory().getId() - 1));
+                        } else {
+                            Toast.makeText(getApplicationContext(), "null", Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "" + response.code(), Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Failed 1", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Item> call, Throwable t) {
+                    Toast.makeText(getApplicationContext(), "Cannot load", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    private void getComponents() {
+        lblToolbar = findViewById(R.id.lbl_toolbar);
+        lblToolbar.setText("Cập nhật thông tin");
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnAddImage = findViewById(R.id.btnAddImage);
+        edtItemName = findViewById(R.id.edtItemName);
+        edtItemDes = findViewById(R.id.edtItemDes);
+        edtItemAddress = findViewById(R.id.edtItemAddress);
+        spPrivacy = findViewById(R.id.spPrivacy);
+        spPrivacy.setPopupBackgroundResource(R.color.white);
+        rmaAPIService = RmaAPIUtils.getAPIService();
+        spCategory = findViewById(R.id.spCategory);
+        spCategory.setPopupBackgroundResource(R.color.white);
+    }
+
+    private void updateItem() {
+        String itemName = edtItemName.getText().toString();
+        String itemDes = edtItemDes.getText().toString();
+        String itemAddress = edtItemAddress.getText().toString();
+        int privacy = spPrivacy.getSelectedItemPosition();
+        int category = spCategory.getSelectedItemPosition();
+        final Map<String, String> jsonBody = new HashMap<String, String>();
+        jsonBody.put("name", itemName);
+        jsonBody.put("description", itemDes);
+        jsonBody.put("address", itemAddress);
+        jsonBody.put("privacy", "" + privacy);
+
+        //fake information
+        jsonBody.put("category", "" + (category + 1));
+//        jsonBody.put("url", urlList.get(0));
+
+        if (authorization != null) {
+            rmaAPIService.updateItem(jsonBody, authorization, itemId).enqueue(new Callback<Object>() {
+
+                @Override
+                public void onResponse(Call<Object> call, Response<Object> response) {
+                    if (response.isSuccessful()) {
+                        if (response.body() != null) {
+                            Toast.makeText(getApplicationContext(), "Updated", Toast.LENGTH_LONG).show();
+                            //go to main
+                            Intent intent = new Intent(context, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "null", Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "" + response.code(), Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Failed 1", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Object> call, Throwable t) {
+                    Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    private void createImageView() {
+        gridLayout = (GridLayout) findViewById(R.id.imageGrid);
+        final ImageView imageView = new ImageView(this);
+
+        //set image
+        if (onClickFlag == -1) {
+            Picasso.with(getApplicationContext()).load(urlList.get(urlList.size() - 1))
+                    .placeholder(R.drawable.no)
+                    .error(R.drawable.loadingimage)
+                    .into(imageView);
+        } else {
+            try {
+                Bitmap bmp = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImages.get(selectedImages.size() - 1));
+                imageView.setImageBitmap(bmp);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickFlag = CHANGE_IMAGE_FLAG;
+                tmpImage = imageView;
+                selectedPosition = imageList.indexOf(imageView);
+                getImageFromGallery();
+            }
+        });
+
+        imageList.add(imageView);
+        gridLayout.addView(imageList.get(imageList.size() - 1));
+
+        ViewGroup.LayoutParams layoutParams = imageList.get(imageList.size() - 1).getLayoutParams();
+        layoutParams.height = IMAGE_SIZE;
+        layoutParams.width = IMAGE_SIZE;
+
+        ViewGroup.MarginLayoutParams marginLayoutParams = new ViewGroup.MarginLayoutParams(layoutParams);
+        marginLayoutParams.bottomMargin = IMAGE_MARGIN_TOP_RIGHT;
+        marginLayoutParams.rightMargin = IMAGE_MARGIN_TOP_RIGHT;
+        imageView.setLayoutParams(layoutParams);
+
+        if (imageList.size() == 10) {
+            btnAddImage.setEnabled(false);
+        }
+    }
+}
