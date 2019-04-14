4815a5a2123cca736a44086f14e9830bfda9c0c0
Giang Tran Hoang <giangthse62424@fpt.edu.vn>
2019-04-14 13:25:36 +0700

    Update Paging Item Show_ Giang Tran Hoang

--- app/src/main/java/com/project/capstone/exchangesystem/constants/AppApi.java
+++ app/src/main/java/com/project/capstone/exchangesystem/constants/AppApi.java
@@ -1,7 +1,7 @@
 package com.project.capstone.exchangesystem.constants;
 
 public class AppApi {
-    public static final String API_URL = "http://172.16.4.20";
+    public static final String API_URL = "http://172.16.1.95";
     public static final String API_PORT = "8080";
     public static final String WEBSERVER = "http://35.247.191.68:3000";
     public static final String TRADING_URL = "http://35.247.191.68:3000";
--- app/src/main/java/com/project/capstone/exchangesystem/fragment/MainItemShowFragment.java
+++ app/src/main/java/com/project/capstone/exchangesystem/fragment/MainItemShowFragment.java
@@ -3,6 +3,9 @@ package com.project.capstone.exchangesystem.fragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import com.project.capstone.exchangesystem.R;
 import com.project.capstone.exchangesystem.activity.DescriptionItemActivity;
@@ -28,17 +32,29 @@ import retrofit2.Response;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 import static android.content.Context.MODE_PRIVATE;
 import static com.facebook.FacebookSdk.getApplicationContext;
 
 
 public class MainItemShowFragment extends Fragment {
+    int pastVisibleItems, visibleItemCount, totalItemCount;
+    GridLayoutManager gridLayoutManager;
     RecyclerView mainRecyclerView;
     ArrayList<Item> itemArrayList;
     ItemAdapter itemAdapter;
     Menu menu;
     EditText txtSearch;
     ImageButton btnSearch;
+    private boolean loading;
+    private ProgressBar marker_progress;
+    View footerView;
+    boolean isLoading;
+    boolean limitData;
+    int page;
+    mHandler mHandler;
+    SharedPreferences sharedPreferences;
+    String authorization;
 
 
     public MainItemShowFragment() {
@@ -52,15 +68,25 @@ public class MainItemShowFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        page = 0;
+        isLoading = false;
+        limitData = false;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        SharedPreferences sharedPreferences = getContext().getSharedPreferences("localData", MODE_PRIVATE);
-        final String authorization = sharedPreferences.getString("authorization", null);
+        sharedPreferences = getContext().getSharedPreferences("localData", MODE_PRIVATE);
+        authorization = sharedPreferences.getString("authorization", null);
         final RmaAPIService rmaAPIService = RmaAPIUtils.getAPIService();
         final View view = inflater.inflate(R.layout.fragment_main_item_show, container, false);
+        gridLayoutManager = new GridLayoutManager(view.getContext(), 2);
+        marker_progress = (ProgressBar) view.findViewById(R.id.marker_progress);
+        LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(LAYOUT_INFLATER_SERVICE);
+        footerView = layoutInflater.inflate(R.layout.progressbar, null);
+        mHandler = new mHandler();
+        getData(page);
+
         txtSearch = view.findViewById(R.id.txtSearch);
         btnSearch = view.findViewById(R.id.btnSearch);
         btnSearch.setOnClickListener(new View.OnClickListener() {
@@ -100,9 +126,37 @@ public class MainItemShowFragment extends Fragment {
             }
         });
         mainRecyclerView.setHasFixedSize(true);
-        mainRecyclerView.setLayoutManager(new GridLayoutManager(view.getContext(), 2));
+        mainRecyclerView.setLayoutManager(gridLayoutManager);
         mainRecyclerView.setAdapter(itemAdapter);
-        GetBrandNewItems();
+        mainRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0) //check for scroll down
+                {
+                    final int visibleThreshold = 2;
+                    int lastItem = gridLayoutManager.findLastCompletelyVisibleItemPosition();
+                    int currentTotalCount = gridLayoutManager.getItemCount();
+                    if (currentTotalCount <= lastItem + visibleThreshold && isLoading == false && limitData == false) {
+                        isLoading = true;
+                        ThreadData threadData = new ThreadData();
+                        threadData.start();
+                    }
+                }
+            }
+
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (!recyclerView.canScrollVertically(1)) {
+                    if (isLoading == false && limitData == false) {
+                        isLoading = true;
+                        ThreadData threadData = new ThreadData();
+                        threadData.start();
+                    }
+                }
+            }
+        });
+//        GetBrandNewItems();
         return view;
     }
 
@@ -144,5 +198,66 @@ public class MainItemShowFragment extends Fragment {
         startActivity(iTimKiem);
     }
 
+
+    public class mHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    marker_progress.setVisibility(View.VISIBLE);
+                    break;
+                case 1:
+                    getData(++page);
+                    isLoading = false;
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    }
+
+    private void getData(int page) {
+
+        RmaAPIService rmaAPIService = RmaAPIUtils.getAPIService();
+        rmaAPIService.getAllItemsWithPrivacy(authorization, page, 4).enqueue(new Callback<List<Item>>() {
+            @Override
+            public void onResponse(Call<List<Item>> call, Response<List<Item>> response) {
+                if (response.isSuccessful()) {
+                    List<Item> itemList = response.body();
+                    if (!itemList.isEmpty()) {
+//                        mainCharityPostAdapter.setfilter(donationPosts);
+                        itemArrayList.addAll(itemList);
+                        itemAdapter.notifyDataSetChanged();
+                        marker_progress.setVisibility(View.GONE);
+                    } else {
+                        limitData = true;
+                        marker_progress.setVisibility(View.GONE);
+                    }
+                } else {
+                    System.out.println("không gọi được");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Item>> call, Throwable t) {
+                System.out.println("vào hàm Failure");
+            }
+        });
+
+    }
+
+    public class ThreadData extends Thread {
+        @Override
+        public void run() {
+            mHandler.sendEmptyMessage(0);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message message = mHandler.obtainMessage(1);
+            mHandler.sendMessage(message);
+            super.run();
+        }
+    }
 
 }
